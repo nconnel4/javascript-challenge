0a9fdb04b3a00c9856090a5212ead555e485a66b
nconnel4 <nconnel4@hotmail.com>
2021-06-26 18:45:22 -0400

    Add app.js from level 1

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,47 @@
-// from data.js
+// reference data file
 var tableData = data;
 
-// YOUR CODE HERE!
+// locate table object
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // clear existing table data
+    tbody.html('');
+
+    // build html table
+    data.forEach((dataRow) => {
+        // add row
+        var row = tbody.append("tr");
+
+        // add values
+        Object.values(dataRow).forEach((value) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    })
+}
+
+// filter data based on date
+function filterData() {
+
+    // get date filter value
+    var date = d3.select("#datetime").property("value");
+
+    // initialize filteredData
+    var filteredData = tableData;
+
+    // check that date field is valid
+    if (date) {
+        // if the date is a non-null / non-empty value, apply the filter
+        filteredData = filteredData.filter(row => row.datetime == date);
+    }
+
+    // rebuild table
+    buildTable(filteredData);
+}
+
+// filter listener event
+d3.selectAll("#filter-btn").on("click", filterData);
+
+// initiate table build
+buildTable(tableData);
