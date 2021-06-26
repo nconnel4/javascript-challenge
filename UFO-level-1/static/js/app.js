c46484f735cf46938106ac64a9fddb88dbf51a94
nconnel4 <nconnel4@hotmail.com>
2021-06-26 16:57:49 -0400

    Add buildTable function

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,25 @@
-// from data.js
+// reference data file
 var tableData = data;
 
-// YOUR CODE HERE!
+// locate table object
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+    // clear existing table data
+    tbody.html('');
+
+    // build html table
+    data.forEach((dataRow) => {
+        // add row
+        var row = tbody.append("tr");
+
+        // add values
+        Object.values(dataRow).forEach((value) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    })
+}
+
+
+buildTable(tableData);
